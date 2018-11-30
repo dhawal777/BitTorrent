91080dfe0746a7de21085b47127d1d8721bd3eed
Dhawal Jain <dhawalvrr@gmail.com>
2018-12-01 01:37:00 +0530

    Tracker2

--- server_2018201065.cpp
+++ server_2018201065.cpp
@@ -19,6 +19,7 @@ using namespace std;
 #include "erase.h"
 #include "erase1.h"
 char buf[BUFSIZE]; 
+void tracker2(string a,int PORT,string log,string b,int PORT2);
 //char *hello = "Hello from server"; 
 void clear_buf()/////////////////////////////////BUFFER CLEAR TO AVOID OVERIDING////////////////////////////////
  {
@@ -149,7 +150,87 @@ valread = read( new_socket , buf, BUFSIZE);
    send(new_socket ,buf, BUFSIZE , 0 );
     
 }
-void tracker1(string a,int PORT,string log)
+void tracker1(string a,int PORT,string log,string b,int PORT2)
+{
+      int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char *sip_1;
+
+             sip_1 = new char[a.length() + 1];
+             strcpy(sip_1,a.c_str());
+       
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                                                  &opt, sizeof(opt))) 
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = inet_addr(sip_1); 
+    address.sin_port = htons( PORT ); 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (bind(server_fd, (struct sockaddr *)&address,  
+                                 sizeof(address))<0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if (listen(server_fd, 3) < 0)   
+    { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    } 
+     while(1)
+     {
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+                       (socklen_t*)&addrlen))<0) 
+    { 
+       
+        perror("accept");
+       thread th5(tracker2,b,PORT2,log,a,PORT); 
+       th5.join();  
+        exit(EXIT_FAILURE); 
+    } 
+      int valread;
+      //char decide[BUFSIZE];
+     valread = read( new_socket ,buf,BUFSIZE); 
+     //isko bataya isko kya karna hai
+    // cout<<"decide"<<endl;
+    // cout<<buf[0]<<endl;
+     if(buf[0]=='2')//GETTING INFORMED ABOUT ITS TASK
+     {thread th1(foo,new_socket,log); 
+        th1.join();
+     }
+    else if(buf[0]=='1')
+    {thread th2(share,new_socket,log);
+        th2.join();
+    }
+    else if(buf[0]=='3')
+    {
+       thread th3(erase,new_socket,log);
+       th3.join();
+    }
+    else if(buf[0]=='4')
+    {
+        thread th4(rm_torrent,new_socket,log);
+         th4.join();
+    }
+ 
+}
+}
+
+void tracker2(string a,int PORT,string log,string b,int PORT2)
 {
       int server_fd, new_socket, valread; 
     struct sockaddr_in address; 
@@ -196,6 +277,8 @@ void tracker1(string a,int PORT,string log)
                        (socklen_t*)&addrlen))<0) 
     { 
         perror("accept"); 
+        thread th6(tracker1,b,PORT2,log,a,PORT); 
+        th6.join();  
         exit(EXIT_FAILURE); 
     } 
       int valread;
@@ -205,22 +288,22 @@ void tracker1(string a,int PORT,string log)
     // cout<<"decide"<<endl;
     // cout<<buf[0]<<endl;
      if(buf[0]=='2')//GETTING INFORMED ABOUT ITS TASK
-     {thread th1(foo,new_socket,log); 
-        th1.join();
+     {thread th7(foo,new_socket,log); 
+        th7.join();
      }
     else if(buf[0]=='1')
-    {thread th2(share,new_socket,log);
-        th2.join();
+    {thread th8(share,new_socket,log);
+        th8.join();
     }
     else if(buf[0]=='3')
     {
-       thread th3(erase,new_socket,log);
-       th3.join();
+       thread th9(erase,new_socket,log);
+       th9.join();
     }
     else if(buf[0]=='4')
     {
-        thread th4(rm_torrent,new_socket,log);
-         th4.join();
+        thread th10(rm_torrent,new_socket,log);
+         th10.join();
     }
  
 }
@@ -279,6 +362,7 @@ int main(int argc, char const *argv[])
       string s_log=argv[4];
      //cout<<ip1<<" "<<x<<endl;
     
-     thread th0(tracker1,ip1,x,s_seed); 
+     thread th0(tracker1,ip1,x,s_seed,ip2,x1); 
       th0.join();  
+    
 }
